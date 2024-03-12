44064cd81c7c9253a5dea87feeb26951f70d41e5
Eclipse <eclipse@eclipseisoffline.xyz>
2024-03-12 18:17:37 +0000

    Fixed custom data being erased on player death, version bump

--- src/main/java/xyz/eclipseisoffline/modifyplayerdata/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/xyz/eclipseisoffline/modifyplayerdata/mixin/ServerPlayerEntityMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.eclipseisoffline.modifyplayerdata.CustomDataHolder;
 import xyz.eclipseisoffline.modifyplayerdata.PlayerNbtModifier;
 
 @Mixin(ServerPlayerEntity.class)
@@ -19,5 +20,10 @@ public class ServerPlayerEntityMixin {
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
     public void writeExtraCustomNbt(NbtCompound nbt, CallbackInfo callbackInfo) {
         PlayerNbtModifier.writeCustomNbt((ServerPlayerEntity) (Object) this, nbt);
+    }
+
+    @Inject(method = "copyFrom", at = @At("TAIL"))
+    public void copyCustomData(ServerPlayerEntity oldPlayer, boolean alive, CallbackInfo callbackInfo) {
+        ((CustomDataHolder) this).modifyPlayerData$setCustomNbtData(((CustomDataHolder) oldPlayer).modifyPlayerData$getCustomNbtData());
     }
 }
